4565e3c52ce06d5e6d6349e07cc8aa101ee7c03f
Niccolò Albanese <nick_albanese@virgilio.it>
2025-01-28 10:47:35 +0100

    dao ok

---
+++ src/main/java/DAO/BigliettoDaoImpl.java
@@ -0,0 +1,50 @@
+package DAO;
+
+import Entities.Biglietto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class BigliettoDaoImpl implements BigliettoDao{
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasportiPU");
+
+	public void salvaBiglietto(Biglietto biglietto){
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(biglietto);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public Biglietto trovaPerCodice(Long codice){
+		EntityManager em = emf.createEntityManager();
+		Biglietto biglietto = em.find(Biglietto.class, codice);
+		em.close();
+		return biglietto;
+	}
+
+	public List<Biglietto> bigliettiEmessiInPeriodo(String startDate, String endDate){
+		EntityManager em = emf.createEntityManager();
+		List<Biglietto> result = em.createQuery("SELECT b FROM Biglietto b WHERE b.dataEmissione BETWEEN " +
+				":start AND :end", Biglietto.class)
+			.setParameter("start", startDate)
+			.setParameter("end", endDate)
+			.getResultList();
+		em.close();
+		return result;
+	}
+
+	public void annullaBiglietto(Long codice){
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Biglietto biglietto = em.find(Biglietto.class, codice);
+		if(biglietto!= null){
+			biglietto.annulla();
+			em.merge(biglietto);
+		}
+		em.getTransaction().commit();
+		em.close();
+	}
+}
